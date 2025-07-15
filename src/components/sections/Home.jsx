d6c2f11e95ba6e34e6eb35b95d8172bf4a54f3e4
starman37 <omarabdullah181628@gmail.com>
2025-07-15 19:25:48 +0600

    Initial project setup and file import

---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,23 @@
+import RevealOnScroll from "../RevealOnScroll"
+
+const Home = () => {
+  return (
+    <section
+        id="home"
+        className="min-h-screen flex items-center justify-center relative"
+    >
+      <RevealOnScroll>
+      <div className="text-center z-10 px-4">
+        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 to-cyan-600 bg-clip-text text-transparent leading-right">Hi, This is Starman Webworks</h1>
+        <p className="text-gray-400 text-lg mb-8 max-w-lg mx-auto">We are front end web development company. Our goal is to build clean and aesthetic websites for you!</p>
+        <div className="flex justify-center space-x-4">
+            <a href="#projects" className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba[(59,130,246,0.2)]]">View Projects</a>
+            <a href="#contact" className="border border-blue-500/50 text-blue-500 py-3 px-6 rounded font-medium transition-all duration-200 hover:-translate-y-0.5 hover:shadow-[0_0_15px_rgba[(59,130,246,0.2)]] hover:bg-blue-500">Contact Us</a>
+        </div>
+      </div>
+      </RevealOnScroll>
+    </section>
+  )
+}
+
+export default Home
